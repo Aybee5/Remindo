dcdab4b50eccac7e66cd954e44ff55064a8d8e3a
Ibrahim Abdullahi Aliyu <ibrahiimaa05@gmail.com>
2020-06-25 10:07:18 +0100

    removes mounted function

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,29 @@
 <template>
   <v-app>
-    <NavBar />
-    <router-view />
+    <v-main>
+      <NavBar />
+      <router-view />
+    </v-main>
+    
   </v-app>
 </template>
 
 <script>
 import NavBar from './components/NavBar'
-import { mapActions, mapGetters } from "vuex";
 export default {
   data(){
     return{
-      sideNav: false
     }
   },
   computed:{
-    ...mapActions(["checkLeastUpcomingTime"]),
-    ...mapGetters(["getUpcomingTodoTime"])
   },
   components:{
     NavBar,
   },
   watch: {
-    getUpcomingTodoTime() {
-      this.$store.dispatch("checkLeastUpcomingTime")
-    }
-  },
-  mounted() {
-    this.$store.dispatch("checkLeastUpcomingTime")
   },
   created() {
     
   }
 }
-</script>>
+</script>

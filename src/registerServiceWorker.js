90cb05102a1d55de6965cb44644f11e27bc17929
Ibrahim Abdullahi Aliyu <ibrahiimaa05@gmail.com>
2020-06-25 20:46:14 +0100

    non persis notify

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -5,33 +5,31 @@ import { register } from 'register-service-worker'
 import localforage from "localforage";
 import { setTimeout } from 'core-js';
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready (registration) {  
-      console.log(registration)
       let showNotifications = () => {
-        // registration.showNotification("You have a new task", {
-        //   body: "You ask me to remind you of this",
-        //   actions: [
-        //     {
-        //       action: "view",
-        //       title: "View",
-        //     },
-        //     {
-        //       action: "discard",
-        //       title: "Discard",
-        //     }
-        //   ],
-        //   requireInteraction: true
-        // })
-        new Notification("From non persis", {
-          body: "body"
-        }) 
+        registration.showNotification("You have a new task", {
+          body: "You ask me to remind you of this",
+          actions: [
+            {
+              action: "view",
+              title: "View",
+            },
+            {
+              action: "discard",
+              title: "Discard",
+            }
+          ],
+          requireInteraction: true
+        })
       }
-      self.addEventListener("notificationclick", evt =>{
-        console.log(evt)
+      self.addEventListener("notificationclick", event=>{
+        event.notification.close()
+        console.log("notification closed",event)
       })
       self.addEventListener("message", event=>{
+        console.log("message receiveds", event)
         if (event.data) {
           localforage.getItem("time").then((data)=>{
             if (!data) return;
@@ -54,6 +52,7 @@ import { setTimeout } from 'core-js';
       console.log('New content is downloading.')
     },
     updated () {
+      self.skipWaiting().then(console.log("skipped"))
       console.log('New content is available; please refresh.')
     },
     offline () {
@@ -63,4 +62,4 @@ import { setTimeout } from 'core-js';
       console.error('Error during service worker registration:', error)
     }
   })
-// }
+}
